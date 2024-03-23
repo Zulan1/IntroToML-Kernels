ecd5ce7245cfce23391298fbbd321b35480c0961
dvirbz <dvirbz@gmail.com>
2024-03-23 17:49:30 +0200

    first implemntation of softsvmpoly

---
+++ analyze_softsvmpoly.py
@@ -0,0 +1,126 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from softsvmpoly import softsvmpoly
+import time
+
+ROUND_DIGITS = 5
+
+def scatter_plot(title, x_label, y_label):
+    """plots a scatter plot with the given parameters"""
+    data = np.load('EX3q2_data.npz')
+    trainX = data['Xtrain']
+    trainy = data['Ytrain']
+    trainX_blue = trainX[trainy == 1]
+    trainX_red = trainX[trainy == -1]
+    print(trainX_blue[:10])
+    plt.scatter(*zip(*trainX_blue), color='blue', label='y=1')
+    plt.scatter(*zip(*trainX_red), color='red', label='y=-1')
+    plt.title(title, fontsize=26)
+    plt.xlabel(x_label, fontsize=16)
+    plt.ylabel(y_label, fontsize=16)
+    plt.grid(True)
+
+def predict_calculate_error(w, trainX, trainy, testX, testy, k):
+    """predicts the testy values and calculates the error"""
+    def decision_function(x):
+        return sum(w[i] * trainy[i] * ((1 + x @ trainX[i]) ** k) for i in range(len(w)))
+    y_preds = np.array([np.sign(decision_function(x)) for x in testX])
+    return np.mean(np.vstack(testy) != np.vstack(y_preds))
+
+def test_input_size(m: int = 200, l: int = 1, k: int = 5):
+    """tests the ssvm algorithm for a given sample size and l value"""
+    # load question 2 data
+    data = np.load('EX3q2_data.npz')
+    trainX = data['Xtrain']
+    testX = data['Xtest']
+    trainy = data['Ytrain']
+    testy = data['Ytest']
+
+    # Get a random m training examples from the training set
+    indices = np.random.permutation(trainX.shape[0])
+    _trainX = trainX[indices[:m]]
+    _trainy = trainy[indices[:m]]
+
+    w = softsvmpoly(l, k, _trainX, _trainy)
+
+    test_error = predict_calculate_error(w, trainX, trainy, testX, testy, k)
+    train_error = predict_calculate_error(w, trainX, trainy, _trainX, _trainy, k)
+
+    return test_error, train_error
+
+def k_fold_cross_validation(k_f: int, l: int, k: int, trainX: np.array, trainy: np.array):
+    m, _ = trainX.shape
+    indices = np.random.permutation(m)
+    fold_size = m // k_f
+    trainX_folds = [trainX[indices[i:i + fold_size]] for i in range(0, m, fold_size)]
+    trainy_folds = [trainy[indices[i:i + fold_size]] for i in range(0, m, fold_size)]
+
+    test_errors = []
+    for i in range(k_f):
+        valX = trainX_folds[i]
+        valY = trainy_folds[i]
+        trainX = np.concatenate([trainX_folds[j] for j in range(k_f) if j != i])
+        trainy = np.concatenate([trainy_folds[j] for j in range(k_f) if j != i])
+        w = softsvmpoly(l, k, trainX, trainy)
+        test_errors.append(predict_calculate_error(w, trainX, trainy, valX, valY, k))
+
+    print(f'k={k}, l={l}, test_errors={test_errors}')    
+    return np.average(test_errors)
+
+def analyze_lambda_k_values(k_f, l_values, k_values, trainX, trainy, testX, testy):
+    """tests the knn algorithm for different k"""   
+    best_k, best_l, best_error = 0, 0, 1
+    for k in k_values:
+        for l in l_values:
+            test_error = k_fold_cross_validation(k_f, l, k, trainX, trainy)
+            best_error, best_k, best_l = min((best_error, best_k, best_l), (test_error, k, l), key=lambda x: x[0])
+            print(f'k={k}, l={l}, test_error={test_error}')
+
+    w = softsvmpoly(best_l, best_k, trainX, trainy)
+
+    test_error = predict_calculate_error(w, trainX, trainy, testX, testy, best_k)
+    print(f'best_k={best_k}, best_l={best_l}, test_error={test_error}')
+    return best_k, best_l, test_error
+
+def plot_error_bar_graph(x_values, averages, yerr, title, x_label, x_scale='linear', color='b', label='error bar'):
+    """plots error bar graph with the given parameters"""
+    # Plotting the graph with error bars
+    plt.errorbar(x_values, averages,
+                 yerr=yerr, fmt='-o',
+                 color=color, ecolor='gray',
+                 capsize=5, label=label,
+                 )
+
+    # Annotating each data point
+    # for i, size in enumerate(x_values):
+    #     plt.annotate(round(averages[i], ROUND_DIGITS),
+    #                 # f'High: {round(errors_above[i] + averages[i], ROUND_DIGITS)}\n'
+    #                 # f'Avg: {round(averages[i], ROUND_DIGITS)}\n'
+    #                 # f'Low: {round(averages[i] - errors_below[i], ROUND_DIGITS)}',
+    #                 (size, averages[i]),
+    #                 textcoords="offset points",
+    #                 xytext=(0, 10),
+    #                 ha='center',
+    #                 fontsize=12)
+
+    plt.title(title, fontsize=26)
+    plt.xlabel(x_label, fontsize=16)
+    plt.ylabel('Error', fontsize=16)
+    plt.xscale(x_scale)
+    plt.xticks(x_values, fontsize=12)
+    plt.grid(True)
+
+if __name__ == '__main__':
+    # load question 2 data
+    data = np.load('EX3q2_data.npz')
+    trainX = data['Xtrain']
+    testX = data['Xtest']
+    trainy = data['Ytrain']
+    testy = data['Ytest']
+    
+    start = time.time()
+    # scatter_plot('Soft SVM Samples scatter', 'X', 'Y')
+    analyze_lambda_k_values(5, [1, 10, 100], [2, 5, 8], trainX, trainy, testX, testy)
+    end = time.time()
+    plt.show()
+    print(f'time_to_process in seconds {end - start}')
--- softsvmpoly.py
+++ softsvmpoly.py
@@ -6,14 +6,33 @@ import matplotlib.pyplot as plt
 # todo: complete the following functions, you may add auxiliary functions or define class to help you
 def softsvmpoly(l: float, k: int, trainX: np.array, trainy: np.array):
     """
-
     :param l: the parameter lambda of the soft SVM algorithm
     :param sigma: the bandwidth parameter sigma of the RBF kernel.
     :param trainX: numpy array of size (m, d) containing the training sample
     :param trainy: numpy array of size (m, 1) containing the labels of the training sample
     :return: numpy array of size (m, 1) which describes the coefficients found by the algorithm
     """
-    raise NotImplementedError()
+    def poly_kernel(x1, x2):
+          return (1 + x1 @ x2) ** k
+    m = int(trainX.shape[0])
+    zeros = spmatrix([], [], [], (m, m))
+    gram_matrix = matrix([[poly_kernel(x1, x2) for x2 in trainX] for x1 in trainX]) + 1e-5 * spdiag([1.0] * m)
+    H = 2*l*matrix(sparse([[gram_matrix, zeros], [zeros, zeros]]))
+    u = matrix([0.0] * m + [1.0/m] * m)
+    v = matrix([1.0] * m + [0.0] * m)
+    Im = spmatrix(1.0, range(m), range(m))
+    Y = matrix(spdiag(list(float(y) for y in trainy)))
+    YG = Y * gram_matrix
+    A = matrix(sparse([[YG, zeros], [Im, Im]]))
+    print(H.size, u.size, A.size, v.size)
+    sol = solvers.qp(H, u, -A, -v)
+    return np.vstack(np.array(sol['x']))[:m]
+
+    # print(A)
+    # np_gram_matrix = np.array(A)
+    # print(np_gram_matrix.shape)
+    # w,_ = np.linalg.eig(np_gram_matrix)
+    # print(len([w_i for w_i in w if w_i < 0]))
 
 
 def simple_test():
@@ -37,8 +56,7 @@ def simple_test():
     # tests to make sure the output is of the intended class and shape
     assert isinstance(w, np.ndarray), "The output of the function softsvmbf should be a numpy array"
     assert w.shape[0] == m and w.shape[1] == 1, f"The shape of the output should be ({m}, 1)"
-
-
+    
 if __name__ == '__main__':
     # before submitting, make sure that the function simple_test runs without errors
     simple_test()
